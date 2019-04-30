5df13379efca66c09241866a1ceb10a5821fc0f3
vncs2012 <vncs2012@gmail.com>
2019-04-30 00:16:11 -0300

    mudanças a coisa esta indo

--- readable/src/components/Comentario.js
+++ readable/src/components/Comentario.js
@@ -1,58 +1,109 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { formatComentario, formatDate } from '../utils/helpers'
-import { MdThumbUp, MdThumbDown,MdClear,MdBrush } from 'react-icons/md/index'
-import { handleComentarioLike,handleGetComentarios,handleDelComentarios } from '../actions/comentario'
+import { MdThumbUp, MdThumbDown, MdClear, MdBrush } from 'react-icons/md/index'
+import { handleComentarioLike, handleGetComentarios, handleDelComentarios } from '../actions/comentario'
 import { handleInitialData } from '../actions/shared'
 
 class Comentario extends Component {
-  state ={
-    edit: false
+  state = {
+    edit: false,
+    body: '',
+    author: '',
   }
+  handleChangeText = (e) => {
+    const body = e.target.value
+
+    this.setState(() => ({
+      body
+    }))
+  }
+  handleChangeAuthor = (e) => {
+    const author = e.target.value
+
+    this.setState(() => ({
+      author
+    }))
+  }
+
   handleLike = (e, opt) => {
     e.preventDefault()
-    const { dispatch, idC ,idp} = this.props
+    const { dispatch, idC, idp } = this.props
     console.log(opt, '---', idC)
     dispatch(handleComentarioLike(idC, opt))
     dispatch(handleGetComentarios(idp))
   }
   handleDel = (e) => {
     e.preventDefault()
-    const { dispatch, idC ,idp} = this.props
+    const { dispatch, idC, idp } = this.props
     dispatch(handleDelComentarios(idC))
     dispatch(handleInitialData())
     dispatch(handleGetComentarios(idp))
+  }
+  handleEdit = (e) => {
+    e.preventDefault()
+    const { body, author } = this.props
+    console.log( this.props)
+    this.setState({
+      edit: true,
+      body,
+      author
+    })
   }
   render() {
     const { comentario } = this.props
     if (comentario === null) {
       return <p>This Comment doesn't existd</p>
     }
-    const { timestamp, body, author, voteScore
-    } = comentario
+    const { timestamp, body, author, voteScore } = comentario
+    console.log(this.state.body)
     return (
-      <div className="card" >
-        <div className="card-body">
-          <h6 className="card-subtitle mb-2 text-muted">
-            author: {author} - {formatDate(timestamp)}
-            <MdClear style={{float: "right"}} className='tweet-icon' onClick={(e) =>this.handleDel(e)} />
-            <MdBrush style={{float: "right"}} className='tweet-icon' />
-          </h6>
-          <p className="card-text">{body}</p>
-          <div className='card-link'>
-            <MdThumbUp className='tweet-icon' onClick={(e) =>this.handleLike(e,'upVote')} />
-            <MdThumbDown className='tweet-icon' onClick={(e) =>this.handleLike(e,'downVote')} />
-            <span>{voteScore !== 0 ? voteScore : 0}</span>
+      <div>
+        {this.state.edit === false ? (
+          <div className="card" >
+            <div className="card-body">
+              <h6 className="card-subtitle mb-2 text-muted">
+                author: {author} - {formatDate(timestamp)}
+                <MdClear style={{ float: "right" }} className='tweet-icon' onClick={(e) => this.handleDel(e)} />
+                <MdBrush style={{ float: "right" }} className='tweet-icon' onClick={(e) => this.handleEdit(e)} />
+              </h6>
+              <p className="card-text">{body}</p>
+              <div className='card-link'>
+                <MdThumbUp className='tweet-icon' onClick={(e) => this.handleLike(e, 'upVote')} />
+                <MdThumbDown className='tweet-icon' onClick={(e) => this.handleLike(e, 'downVote')} />
+                <span>{voteScore !== 0 ? voteScore : 0}</span>
+              </div>
+            </div>
           </div>
-        </div>
+        ) : (
+            <div>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className='card-title'>Edite Comentario</h5>
+                  <form className='new-tweet' onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                      <input className="form-control form-control-sm" value={author} type="text" onChange={this.handleChangeAuthor} placeholder='enter your name'></input>
+                    </div>
+                    <textarea className="form-control"
+                      placeholder="What are you thinking about post?"
+                      value={body}
+                      onChange={this.handleChangeText}
+                      className='textarea'
+                    />
+                    <button type="submit" className="btn btn-primary">Primary</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          )}
       </div>
     )
   }
 }
-function mapStateToProps({ comentario }, { idC, idp }) {
+function mapStateToProps({ comentario }, { idC }) {
   return {
     comentario: comentario[idC]
-      ? formatComentario(comentario[idC], idp)
+      ? formatComentario(comentario[idC])
       : null
   }
 }
